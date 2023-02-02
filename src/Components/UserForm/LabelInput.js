915223f86765d62cf2558787d8180f921e3317ad
kishan kathare <kishankathare45@gmail.com>
2023-02-03 01:34:13 +0530

    Access to value to input fields added

--- src/Components/UserForm/LabelInput.js
+++ src/Components/UserForm/LabelInput.js
@@ -5,7 +5,12 @@ const LabelInput = (props) => {
     return (
         <div className={styles["input-label"]}>
             <label htmlFor={props.name}>{props.title}</label>
-            <input type={props.type} id={props.name}/>
+            <input 
+                type={props.type} 
+                id={props.name} 
+                value={props.value} 
+                onChange={(event) => {props.onChange(event)}}
+            />
         </div>
     )
 }
--- src/Components/UserForm/UserForm.js
+++ src/Components/UserForm/UserForm.js
@@ -1,12 +1,44 @@
+import { useState } from 'react';
 
 import LabelInput from './LabelInput';
 import Button from '../UI/Button/Button';
 
 const UserForm = () => {
+
+    const [name, setName] = useState('');
+    const [age, setAge] = useState(0);
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleAgeChange = (event) => {
+        setAge(event.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(name, age);
+        setName('')
+        setAge(0)
+    }
+
     return(
-        <form>
-            <LabelInput type='text' name='name' title="Username: "/>
-            <LabelInput type='number' name='age' title="Age(in years): "/>
+        <form onSubmit={handleSubmit}>
+            <LabelInput 
+                type='text' 
+                name='name' 
+                title="Username: "
+                value={name}
+                onChange={handleNameChange}
+            />
+            <LabelInput 
+                type='number' 
+                name='age' 
+                title="Age(in years): "
+                value={age}
+                onChange={handleAgeChange}
+            />
             <Button type="submit">Add User</Button>
         </form>
     );
